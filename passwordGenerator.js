12753cc496c6df2e4c6b4180f92f4110bbd9600f
Rayyan-Alam71 <rayyanalam752@gmail.com>
2024-08-26 01:01:03 +0530

    Update passwordGenerator.js

--- passwordGenerator.js
+++ passwordGenerator.js
@@ -7,35 +7,21 @@ const uppercase ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowercase ="abcdefghijklmnopqrstuvwxyz";
 const signs = "!@#$%&*/|~{}[]";
 const digits = "0123456789"
-const allChars = uppercase + lowercase + signs;
+const allChars = uppercase + lowercase + signs + digits;
 
 
 function generatePassword(){
     let length = document.getElementById("passwordLength").value;
-    length = Number(length);
-    // console.log(typeof(length));
-
-
-    
-
     input.textContent ="";
     let password ="";
-    // length = length.textContent;
-    // password += uppercase[Math.floor(Math.random() * uppercase.length)];
-    // password += lowercase[Math.floor(Math.random() * lowercase.length)];
-    // password += signs[Math.floor(Math.random() * signs.length)];
-    // password += digits[Math.floor(Math.random() * digits.length)];
 
     while(length > password.length){
         password += allChars[Math.floor(Math.random() * allChars.length)];
     }
-    // console.log(password.length)
-    // console.log("entered length:", length)
     input.value = password;
     disablePassField = document.getElementById("passwordField").disabled = true;
 }
 document.addEventListener("keydown", (e)=>{
-    // console.log(e.key);
     if(e.key == "Enter"){
         generatePassword();
     }
